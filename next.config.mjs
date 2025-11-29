b9628241e4961eb0a27e9ca4636ce39e6b313894
sampad24 <96785596+sampad24@users.noreply.github.com>
2025-11-29 22:54:53 +0530

    changed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import { withNextVideo } from "next-video/process";
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextVideo(nextConfig);
